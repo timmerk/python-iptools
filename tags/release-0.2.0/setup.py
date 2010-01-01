cd2583b3f8f08099263670fa67a311454ce70df2
casadebender <casadebender@99efc558-b41a-11dd-8714-116ca565c52f>
2010-01-01 06:36:36 +0000

    tagging 0.2.0 release

---
+++ tags/release-0.2.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+version = '0.2'
+
+setup(
+    name = 'iptools',
+    version = version,
+    description = 'Python utilites for manipulating IP addresses',
+    long_description = "Utilities for manipulating IP addresses including a class that can be used to include CIDR network blocks in Django's INTERNAL_IPS setting.",
+    url = 'http://python-iptools.googlecode.com',
+    download_url = '',
+    author = 'Bryan Davis',
+    author_email = 'casadebender+iptools@gmail.com',
+    license = 'BSD',
+    platforms = ['any',],
+    package_dir = {'': 'src'},
+    packages = find_packages('src'),
+    include_package_data = True,
+    classifiers = [
+      'Development Status :: 3 - Alpha', 
+      'Environment :: Web Environment',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Topic :: Utilities',
+      'Topic :: Internet', 
+    ],
+    zip_safe=False,
+)
